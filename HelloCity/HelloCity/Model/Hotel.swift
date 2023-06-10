4c976cb2013a871d992c1c24252e823df81da5bd
freemjstudio <mjwoo0@naver.com>
2023-06-11 00:17:01 +0900

    [ADD] create models

---
+++ HelloCity/HelloCity/Model/Hotel.swift
@@ -0,0 +1,8 @@
+//
+//  Hotel.swift
+//  HelloCity
+//
+//  Created by 우민지 on 2023/06/10.
+//
+
+import Foundation
---
+++ HelloCity/HelloCity/Model/Place.swift
@@ -0,0 +1,8 @@
+//
+//  Place.swift
+//  HelloCity
+//
+//  Created by 우민지 on 2023/06/11.
+//
+
+import Foundation
