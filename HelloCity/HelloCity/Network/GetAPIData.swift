79be2bc7ea4385f7db4b18a0acc844ec8082f9b7
freemjstudio <mjwoo0@naver.com>
2023-06-11 00:17:31 +0900

    [ADD] create models

--- HelloCity/HelloCity/Network/GetAPIData.swift
+++ HelloCity/HelloCity/Network/GetAPIData.swift
@@ -19,20 +19,24 @@ class GetAPIData {
         let dataRequset = AF.request(url,
                                      method: .get,
                                      parameters: nil).validate(statusCode: 200..<300)
+        
+        var hotelList:[Hotel] = []
         dataRequset.responseJSON() { response in
             switch response.result {
             case .success(let value):
                 if let jsonData = value as? [String:Any]{
                     print("Data count : \(jsonData.count)")
                     
+                    if let 
                     // 여기서 부터 jsonData['title'] 이런식으로 데이터 얻어오기
-                    
+                    // hotelList.append()
                 }
             case .failure(let error):
                 print("error: \(String(describing: error.errorDescription))")
             }
         }
-       
+        
+
     }
     
 }
