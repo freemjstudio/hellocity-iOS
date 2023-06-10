9e3207ca969022257c2e00dd2dd436b7e8ec0bac
freemjstudio <mjwoo0@naver.com>
2023-06-10 21:53:09 +0900

    Alamofire 설치 + GetAPIData Logic

---
+++ HelloCity/HelloCity/Network/GetAPIData.swift
@@ -0,0 +1,38 @@
+//
+//  GetAPIData.swift
+//  HelloCity
+//
+//  Created by 우민지 on 2023/06/10.
+//
+
+import Foundation
+import Alamofire
+
+class GetAPIData {
+    static let shared = GetAPIData()
+    
+    private init() {}
+    
+    func getHotelList() {
+        let url = APIConstants.hotelURL
+        
+        let dataRequset = AF.request(url,
+                                     method: .get,
+                                     parameters: nil).validate(statusCode: 200..<300)
+        dataRequset.responseJSON() { response in
+            switch response.result {
+            case .success(let value):
+                if let jsonData = value as? [String:Any]{
+                    print("Data count : \(jsonData.count)")
+                    
+                    // 여기서 부터 jsonData['title'] 이런식으로 데이터 얻어오기
+                    
+                }
+            case .failure(let error):
+                print("error: \(String(describing: error.errorDescription))")
+            }
+        }
+       
+    }
+    
+}
